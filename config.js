465b7122d27bd6f17a4b6f9ca55ec3f2817df66d
macbook <macbook@macbooks-MacBook-Air.local>
2021-10-22 15:07:05 +0700

    init file

---
+++ config.js
@@ -0,0 +1,17 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào thân ái và quyết thắng🖐🖐🖐',
+    introDesc: `ấn ok để bước vào thế giới của tui nha ^^`,
+    btnIntro: 'OK',
+    title: 'muốn dắt tui đi chơi lắm đúng khum ?',
+    desc: 'hong trả lời mà thoát ra tức là đồng ý nhaaa ',
+    btnYes: 'chời ưi đúng rùi',
+    btnNo: 'khùm hả Ánh -.-',
+    question:'Nói cho tui biết lí do đi nè :>>',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Ánh quá là dễ thw hehee',
+    mess: 'tui biết mà, ai cũng nói vậy hết trơn :))',
+    messDesc: '💬💬 liên lạc với tui nhaa💬💬',
+    btnAccept: 'Ok lun 💖'
+}
